9e03ab70580784938898b9930f4817ed1cdf3830
Kalvin Krajca <kalman98@kalman98.net>
2017-03-10 14:54:25 -0600

    Added color to battle names, so fights can make more sense.

--- world/rules.py
+++ world/rules.py
@@ -26,55 +26,55 @@ def resolve_combat(combat_handler, actiondict):
                 if not exchanges[tchar] == char:
                     if action == "hit":
                         if taction == "parry" and ttarget == char:
-                            msg = "%s tries to hit %s, but %s parries the attack!"
+                            msg = "|R%s|W tries to hit |G%s|W, but |G%s|W parries the attack!"
                             messages.append(msg % (char, tchar, tchar))
                         elif taction == "defend" and random < 0.5:
-                            msg = "%s defends against the attack by %s."
+                            msg = "|G%s|W defends against the attack by |R%s|W."
                             messages.append(msg % (tchar, char))
                         elif taction == "flee":
-                            msg = "%s stops %s from disengaging, with a hit!"
+                            msg = "|G%s|W stops |R%s|W from disengaging, with a hit!"
                             flee[tchar] = -2
                             messages.append(msg % (char, tchar))
                         else:
-                            msg = "%s hits %s, bypassing their %s!"
+                            msg = "|G%s|W hits |R%s|W, bypassing their %s!"
                             messages.append(msg % (char, tchar, taction))
                     elif action == "parry":
                         if taction == "hit":
-                            msg = "%s parries the attack by %s."
+                            msg = "|G%s|W parries the attack by |R%s|W."
                             messages.append(msg % (char, tchar))
                         elif taction == "feint":
-                            msg = "%s tries to parry, but %s feints and hits!"
+                            msg = "|R%s|W tries to parry, but |G%s|W feints and hits!"
                             messages.append(msg % (char, tchar))
                         else:
-                            msg = "%s attempts to parry %s, to no avail."
+                            msg = "|R%s|W attempts to parry |G%s|W, to no avail."
                             messages.append(msg % (char, tchar))
                     elif action == "feint":
                         if taction == "parry":
-                            msg = "%s feints past %s's parry, landing a hit!"
+                            msg = "|G%s|W feints past |R%s|W's parry, landing a hit!"
                             messages.append(msg % (char, tchar))
                         elif taction == "hit":
-                            msg = "%s feints but is defeated by %s hit!"
+                            msg = "|R%s|W feints but is defeated by |G%s|W hit!"
                             messages.append(msg % (char, tchar))
                         else:
-                            msg = "%s attepmts to feint %s, to no avail."
+                            msg = "|R%s|W attempts to feint |G%s|W, to no avail."
                             messages.append(msg % (char, tchar))
             # This goes here so that it will run regardless of if a target is set - which there shouldn't be.
             elif action == "defend":
-                msg = "%s defends."
+                msg = "|w%s|W defends."
                 messages.append(msg % char)
             elif action == "flee":
                 if char in flee:
                     flee[char] += 1
                 else:
                     flee[char] = 1
-                msg = "%s tries to disengage (two subsequent turns needed)"
+                msg = "|w%s|W tries to disengage (two subsequent turns needed)"
                 messages.append(msg % char)
 
         # echo results of each subturn
         combat_handler.msg_all("\n".join(messages))
 
     # at the end of both sub-turns, test if anyone fled
-    msg = "%s withdraws from combat."
+    msg = "|w%s|W withdraws from combat."
     for (char, fleevalue) in flee.items():
         if fleevalue == 2:
             combat_handler.msg_all(msg % char)
